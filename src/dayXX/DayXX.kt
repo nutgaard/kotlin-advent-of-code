158e7e2b33b6e96a54193e0c7b7b698dce1d9530
Nicklas Utgaard <nutgaard@gmail.com>
2023-12-02 15:15:06 +0100

    add day1 solution and template for future days

---
+++ src/dayXX/DayXX.kt
@@ -0,0 +1,27 @@
+package dayXX
+
+import println
+import readInput
+import verifySolution
+
+const val dir = "dayXX"
+fun main() {
+    val testInputPart1 = readInput("${dir}/Part01_test")
+    val testInputPart2 = readInput("${dir}/Part02_test")
+
+    verifySolution(testInputPart1, 0, ::part1)
+    verifySolution(testInputPart2, 0, ::part2)
+
+    val input = readInput("$dir/Input")
+
+    part1(input).println()
+    part2(input).println()
+}
+
+fun part1(input: List<String>): Int {
+    return -1
+}
+
+fun part2(input: List<String>): Int {
+    return -1
+}

63a37b98c3f6083687b8bfb4d2e809a1143565f0
Nicklas Utgaard <nutgaard@gmail.com>
2023-12-04 09:59:24 +0100

    update day template

--- src/dayXX/DayXX.kt
+++ src/dayXX/DayXX.kt
@@ -9,8 +9,8 @@ fun main() {
     val testInputPart1 = readInput("${dir}/Part01_test")
     val testInputPart2 = readInput("${dir}/Part02_test")
 
-    verifySolution(testInputPart1, 0, ::part1)
-    verifySolution(testInputPart2, 0, ::part2)
+    verifySolution(testInputPart1, -1, ::part1)
+    verifySolution(testInputPart2, -1, ::part2)
 
     val input = readInput("$dir/Input")
 

713d340910984dc2bde9a5dc97027137eb98078c
Nicklas Utgaard <nutgaard@gmail.com>
2023-12-05 20:13:39 +0100

    add day 5 solution

--- src/Utils.kt
+++ src/Utils.kt
@@ -2,6 +2,7 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+import kotlin.time.measureTimedValue
 
 /**
  * Reads lines from the given input txt file.
@@ -21,8 +22,14 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
 fun Any?.println() = println(this)
 
 fun <INPUT, RES> verifySolution(input: INPUT, expectedResult: RES, algo: (INPUT) -> RES) {
-    val result = algo(input)
+    val result = timed("verifySolution") { algo(input) }
     check(result == expectedResult) {
         "Expected $expectedResult, but got $result"
     }
+}
+
+fun <OUT> timed(name: String, fn: () -> OUT): OUT {
+    val result = measureTimedValue(fn)
+    println("[$name] Calculated in ${result.duration}")
+    return result.value
 }

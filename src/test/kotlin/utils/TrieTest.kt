f2befa3504a7e95e63a699faf7165429cd4bbfae
Nicklas Utgaard <nutgaard@gmail.com>
2024-12-20 16:29:25 +0100

    add dummy test for testing

---
+++ src/test/kotlin/utils/TrieTest.kt
@@ -0,0 +1,11 @@
+package utils
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class TrieTest {
+    @Test
+    fun testit() {
+        assertNotNull("null")
+    }
+}
